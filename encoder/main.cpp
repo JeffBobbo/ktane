4b74c63e32e8309658759f185652c6becedb6dd2
Jeff Bobbo <JeffBobbo@noreply.github.com>
2020-01-11 13:10:25 +0000

    Move Debounce class into shared file

--- encoder/main.cpp
+++ encoder/main.cpp
@@ -1,5 +1,6 @@
 #include "shared/module.h"
 #include "shared/util.h"
+#include "shared/debounce.h"
 
 #include <Encoder.h>
 
@@ -38,61 +39,6 @@ void segment(const uint8_t v)
   shiftOut(PIN_DATA, PIN_CLOCK, MSBFIRST, v == 255 ? 0 : SEG_TABLE[(v / 10) % 10]);
   digitalWrite(PIN_LATCH, 1);
 }
-
-
-class Debounce
-{
-public:
-  Debounce(const uint8_t p, const bool low = false) : pin(p), active_low(low), state(0)
-  {
-    pinMode(pin, INPUT);
-  }
-
-  inline uint8_t getPin() const { return pin; }
-
-  void update() {
-    state = state << 1 | (active_low ? !digitalRead(pin) : digitalRead(pin));
-  }
-
-  bool is_pressed()
-  {
-    const uint8_t MASK = 0b11000111;
-    if ((state & MASK) == 0b00000111)
-    {
-      state = 0b11111111;
-      return true;
-    }
-    return false;
-  }
-
-  bool is_released()
-  {
-    const uint8_t MASK = 0b11000111;
-    if ((state & MASK) == 0b11000000)
-    {
-      state = 0b00000000;
-      return true;
-    }
-    return false;
-  }
-
-  uint8_t is_down() const
-  {
-    return state == 0b11111111;
-  }
-  uint8_t is_up() const
-  {
-    return state == 0b00000000;
-  }
-
-private:
-  const uint8_t pin;
-  const bool active_low;
-  uint8_t state;
-};
-
-
-
 
 const uint8_t CODE_LENGTH = 3;
 int8_t code[CODE_LENGTH] = {0};
---
+++ shared/debounce.h
@@ -0,0 +1,57 @@
+#ifndef DEBOUNCE_H_INCLUDE
+#define DEBOUNCE_H_INCLUDE
+
+#include <Arduino.h>
+
+class Debounce
+{
+public:
+  Debounce(const uint8_t p, const bool low = false) : pin(p), active_low(low), state(0)
+  {
+    pinMode(pin, INPUT);
+  }
+
+  inline uint8_t getPin() const { return pin; }
+
+  void update() {
+    state = state << 1 | (active_low ? !digitalRead(pin) : digitalRead(pin));
+  }
+
+  bool is_pressed()
+  {
+    const uint8_t MASK = 0b11000111;
+    if ((state & MASK) == 0b00000111)
+    {
+      state = 0b11111111;
+      return true;
+    }
+    return false;
+  }
+
+  bool is_released()
+  {
+    const uint8_t MASK = 0b11000111;
+    if ((state & MASK) == 0b11000000)
+    {
+      state = 0b00000000;
+      return true;
+    }
+    return false;
+  }
+
+  uint8_t is_down() const
+  {
+    return state == 0b11111111;
+  }
+  uint8_t is_up() const
+  {
+    return state == 0b00000000;
+  }
+
+private:
+  const uint8_t pin;
+  const bool active_low;
+  uint8_t state;
+};
+
+#endif
