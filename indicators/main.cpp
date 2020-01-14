4650593808ac742912df51a8425ee3d04cbc7693
JeffBobbo <jeffbobbo@users.noreply.github.com>
2020-01-14 22:13:00 +0000

    Indicators updates, wiring changes and double digit display for numerical indicator

--- indicators/main.cpp
+++ indicators/main.cpp
@@ -8,8 +8,8 @@ const uint8_t PIN_STRIKE_1 = 3;
 const uint8_t PIN_STRIKE_2 = 4;
 
 const uint8_t PIN_DATA  = 5;
-const uint8_t PIN_LATCH = 6;
-const uint8_t PIN_CLOCK = 7;
+const uint8_t PIN_CLOCK = 6;
+const uint8_t PIN_LATCH = 7;
 
 const uint8_t PIN_DEFUSED = 8;
 
@@ -20,12 +20,31 @@ const uint8_t PIN_BINARY_3 = 12;
 
 const uint8_t PIN_EXPLODED = 13;
 
-const uint8_t SEG_TABLE[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71};
+//const uint8_t SEG_TABLE[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71};
+const uint8_t SEG_TABLE[] = {
+  B11000000, //0
+  B11111001, //1
+  B10100100, //2
+  B10110000, //3
+  B10011001, //4
+  B10010010, //5
+  B10000010, //6
+  B11111000, //7
+  B10000000, //8
+  B10011000 //9
+};
 
+// void segment(const uint8_t v)
+// {
+//   digitalWrite(PIN_LATCH, 0);
+//   shiftOut(PIN_DATA, PIN_CLOCK, MSBFIRST, v == 255 ? 0 : SEG_TABLE[v & 0x0F]);
+//   digitalWrite(PIN_LATCH, 1);
+// }
 void segment(const uint8_t v)
 {
   digitalWrite(PIN_LATCH, 0);
-  shiftOut(PIN_DATA, PIN_CLOCK, MSBFIRST, v == 255 ? 0 : SEG_TABLE[v & 0x0F]);
+  shiftOut(PIN_DATA, PIN_CLOCK, MSBFIRST, v == 255 ? 0 : SEG_TABLE[v % 10]);
+  shiftOut(PIN_DATA, PIN_CLOCK, MSBFIRST, v == 255 ? 0 : SEG_TABLE[(v / 10) % 10]);
   digitalWrite(PIN_LATCH, 1);
 }
 
